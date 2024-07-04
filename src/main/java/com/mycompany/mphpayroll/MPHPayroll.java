dc619a911ff02b945a18fdc1614a557d26e50444
jaypeegaming <lr.jpbalmaceda@mmdc.mcl.edu.ph>
2024-07-04 22:54:13 +0800

    HOMEWORK #6: MotorPH employee application login functionality
    HOMEWORK #6: MotorPH employee application login functionality

---
+++ src/main/java/com/mycompany/mphpayroll/MPHPayroll.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mphpayroll;
+
+/**
+ *
+ * @author AXISMEG
+ */
+public class MPHPayroll {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
